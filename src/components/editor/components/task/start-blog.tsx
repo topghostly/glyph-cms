01e15243521a17c83116e37da40e44d7742aa71a
David <topghostly@gmail.com>
2025-04-19 00:48:07 +0100

    Fixed bug

--- src/components/editor/components/task/start-blog.tsx
+++ src/components/editor/components/task/start-blog.tsx
@@ -3,29 +3,33 @@ import { useBlogStore } from "@/store/blog-store";
 import { Plus } from "lucide-react";
 
 export const StartBlog: React.FC = () => {
-    const addBlog = useBlogStore((state) => state.addBlog)
-    const setActiveBlog = useBlogStore((state) => state.setActiveBlog)
-    const setActiveTask = useBlogStore((state) => state.setActiveTask)
-    return (
-        <div className="w-full h-full flex justify-center items-center">
-
-            <Button size={"lg"} variant={"outline"} onClick={() => {
-                const newBlogID = crypto.randomUUID()
-                addBlog({
-                    _localID: newBlogID,
-                    content: {
-                        title: "Untitled Blog",
-                    },
-                })
-                setActiveBlog(newBlogID)
-                setActiveTask("structure")
-            }} className="cursor-pointer">
-                <span className="flex gap-1 justify-between items-center text-[14px] text-white/80">
-                    <Plus size={24} strokeWidth={1} />
-                    Create
-                </span>
-            </Button>
-
-        </div>
-    )
-}
+  const addBlog = useBlogStore((state) => state.addBlog);
+  const setActiveBlog = useBlogStore((state) => state.setActiveBlog);
+  const setActiveTask = useBlogStore((state) => state.setActiveTask);
+  return (
+    <div className="w-full h-full flex justify-center items-center">
+      <Button
+        size={"lg"}
+        variant={"outline"}
+        onClick={() => {
+          const newBlogID = crypto.randomUUID();
+          addBlog({
+            _localID: newBlogID,
+            content: {
+              title: "Untitled Blog",
+            },
+            creator: localStorage.getItem("localUserId") ?? "Unknown",
+          });
+          setActiveBlog(newBlogID);
+          setActiveTask("structure");
+        }}
+        className="cursor-pointer"
+      >
+        <span className="flex gap-1 justify-between items-center text-[14px] text-white/80">
+          <Plus size={24} strokeWidth={1} />
+          Create
+        </span>
+      </Button>
+    </div>
+  );
+};
