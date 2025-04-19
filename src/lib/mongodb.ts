ce052d4fc208eeec4ace8f08b275848b0dc9bca5
David <topghostly@gmail.com>
2025-04-19 10:07:49 +0100

    fix: update MongoDB connection environment variable from MONGODB_URI to MONGODB

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectToDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI as string);
+    await mongoose.connect(process.env.MONGODB as string);
     console.log("Connected to database");
   } catch (error) {
     console.log("Couldn't connect to database: ", error);
